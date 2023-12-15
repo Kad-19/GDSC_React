40721a7d254df725aedd56ea27d024a8f818b23c
Kad-19 <kidusasebe1919@gmail.com>
2023-12-15 20:24:10 +0300

    Advanced to do list app with react

---
+++ ToDoList/src/Components/TaskForm.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import "./TaskForm.css";
+import Tag from "./Tag";
+
+const TaskForm = ({ setTasks }) => {
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+    tags: [],
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTasks((prev) => {
+      return [...prev, taskData];
+    });
+    setTaskData({
+        task: "",
+        status: "todo",
+        tags: [],
+      })
+  };
+
+  const checkTag = (tag) => {
+    return taskData.tags.some((item) => item === tag);
+  };
+
+  const selectTag = (tag) => {
+    if (taskData.tags.some((item) => item === tag)) {
+      const filterTags = taskData.tags.filter((item) => item !== tag);
+      setTaskData((prev) => {
+        return { ...prev, tags: filterTags };
+      });
+    } else {
+      setTaskData((prev) => {
+        return { ...prev, tags: [...prev.tags, tag] };
+      });
+    }
+  };
+
+  return (
+    <header className="app_header">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          value={taskData.task}
+          className="task_input"
+          placeholder="Enter your task"
+          onChange={handleChange}
+        />
+
+        <div className="task_form_bottom_line">
+          <div>
+            <Tag
+              tagName="Urgent"
+              selectTag={selectTag}
+              selected={checkTag("Urgent")}
+            />
+            <Tag
+              tagName="Important"
+              selectTag={selectTag}
+              selected={checkTag("Important")}
+            />
+          </div>
+          <div>
+            <select
+              className="task_status"
+              name="status"
+              value={taskData.status}
+              onChange={handleChange}
+            >
+              <option value="todo">To do</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
+
+            <button type="submit" className="task_submit">
+              + Add Task
+            </button>
+          </div>
+        </div>
+      </form>
+    </header>
+  );
+};
+
+export default TaskForm;
